1780e0acc3368dc0e5da5a47901e63e3969cc5ac
Luis <lespinerua@gmail.com>
2025-04-02 11:56:14 -0400

    fix: config

--- cli/tek_secrets/core/config.py
+++ cli/tek_secrets/core/config.py
@@ -6,7 +6,9 @@ from starlette.datastructures import Secret
 load_dotenv(".env")
 
 
-API_URL = os.getenv("API_URL", "Tek Secrets API")
-CLIENT_ID = Secret(os.getenv("CLIENT_ID"))
+# API_URL = os.getenv("API_URL", "Tek Secrets API")
+# CLIENT_ID = Secret(os.getenv("CLIENT_ID"))
+CLIENT_ID = "Ov23liDWHyI85oFOk2b8"
+API_URL = "https://tek-secrets.onrender.com"
 GITHUB_AUTH_URL = "https://github.com/login/oauth/authorize"
 REDIRECT_URI = "http://localhost:8080"
