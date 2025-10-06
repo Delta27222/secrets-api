145c96710e3c87e1424b20fd4d9bc8cfd4b65979
Delta27222 <angelgabriel.hernandez@gmail.com>
2025-10-05 20:32:52 -0400

    feat: ✨ add LoggingMiddleware to enhance request logging in FastAPI application

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from .core.logging import LoggingMiddleware
+
 from .api.v1.api import router as api_router
 from .db.mongodb_utils import close_mongo_connection, connect_to_mongo
 
@@ -16,6 +18,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(LoggingMiddleware)
 app.add_event_handler("startup", connect_to_mongo)
 app.add_event_handler("shutdown", close_mongo_connection)
 app.include_router(api_router, prefix='/v1')
