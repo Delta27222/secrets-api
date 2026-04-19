408893b2520bb416706b3236a558b36153c00b41
angel.hernandez@sabatransporte.com <angelgabriel.hernandez@gmail.com>
2026-04-18 21:17:37 -0400

    feat: ✨ add startup and shutdown event handlers for MongoDB connection and index creation in FastAPI

--- api/app/main.py
+++ api/app/main.py
@@ -1,10 +1,12 @@
+import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .core.logging import LoggingMiddleware
-
 from .api.v1.api import router as api_router
-from .db.mongodb_utils import close_mongo_connection, connect_to_mongo
+from .db.mongodb_utils import close_mongo_connection, connect_to_mongo, create_indexes
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI(title='Tek Secrets')
 origins = [
@@ -19,6 +21,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.add_middleware(LoggingMiddleware)
-app.add_event_handler("startup", connect_to_mongo)
-app.add_event_handler("shutdown", close_mongo_connection)
+
+@app.on_event("startup")
+async def on_startup():
+    """Ejecuta al iniciar la aplicación."""
+    await connect_to_mongo()
+    await create_indexes()
+    logger.info("Aplicación iniciada. Índices de base de datos creados.")
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    """Ejecuta al cerrar la aplicación."""
+    await close_mongo_connection()
 app.include_router(api_router, prefix='/v1')
