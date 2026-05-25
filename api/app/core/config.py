eba61957422999efd4f5ac65f05dcd7263df1ef1
angel.hernandez@sabatransporte.com <angelgabriel.hernandez@gmail.com>
2026-05-25 19:54:01 -0400

    feat: ✨ update configuration to support environment variables for sensitive data, including MongoDB CSFLE master key and AWS credentials, enhancing security and flexibility

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -10,38 +10,41 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # one week
 
 load_dotenv(".env")
 
+# CSFLE Master Key para encriptar/desencriptar los datos en la base de datos
+MONGODB_CSFLE_MASTER_KEY = os.getenv("MONGODB_CSFLE_MASTER_KEY", "")
+
 MAX_CONNECTIONS_COUNT = int(os.getenv("MAX_CONNECTIONS_COUNT", 10))
 MIN_CONNECTIONS_COUNT = int(os.getenv("MIN_CONNECTIONS_COUNT", 10))
-SECRET_KEY = Secret(str(os.getenv("SECRET_KEY", "secret key for project")))
+SECRET_KEY = Secret(str(os.getenv("SECRET_KEY", "")))
+
 # Github
 GITHUB_CLIENT_ID = Secret(
-    str(os.getenv("GITHUB_CLIENT_ID", "Github Client ID")))
+    str(os.getenv("GITHUB_CLIENT_ID", "")))
 GITHUB_CLIENT_SECRET = Secret(
-    str(os.getenv("GITHUB_CLIENT_SECRET", "Github Client Secret")))
-GITHUB_CLIENT_ID_APP = Secret(
-    str(os.getenv("GITHUB_CLIENT_ID_APP", "Github Client ID App")))
-GITHUB_CLIENT_SECRET_APP = Secret(
-    str(os.getenv("GITHUB_CLIENT_SECRET_APP", "Github Client Secret App")))
+    str(os.getenv("GITHUB_CLIENT_SECRET", "")))
+
+GITHUB_ORG_NAME = os.getenv("GITHUB_ORG_NAME", "")
 
 # Database
-PROJECT_NAME = os.getenv("PROJECT_NAME", "Tek Secrets API")
+PROJECT_NAME = os.getenv("PROJECT_NAME", "")
 ALLOWED_HOSTS = CommaSeparatedStrings(os.getenv("ALLOWED_HOSTS", ""))
-MONGO_DB = os.getenv("MONGO_DB", "dev")
-MONGODB_URL = os.getenv("MONGODB_URL", "MongoDB URL")  # deploying without docker-compose
+MONGO_DB = os.getenv("MONGO_DB", "")
+
+MONGODB_URL = os.getenv("MONGODB_URL", "")
 
 # Deployments
-RENDER_API_URL = os.getenv("RENDER_API_URL", " Render API URL")
-VERCEL_API_URL = os.getenv("VERCEL_API_URL", " Vercel API URL")
+RENDER_API_URL = os.getenv("RENDER_API_URL", "")
+VERCEL_API_URL = os.getenv("VERCEL_API_URL", "")
 
 # AWS SQS
-SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL", " SQS Queue URL")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", " AWS Access Key ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", " AWS Secret Access Key")
-AWS_REGION_NAME = os.getenv("AWS_REGION_NAME", "AWS Region Name")
+SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL", "")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
+AWS_REGION_NAME = os.getenv("AWS_REGION_NAME", "")
 
 # QuestDB Information Configuration for Logs
-EC2_INSTANCE_IP = os.getenv("EC2_INSTANCE_IP", "EC2 Instance IP")
-EC2_INSTANCE_PORT = int(os.getenv("EC2_INSTANCE_PORT", 9000))
+EC2_INSTANCE_IP = os.getenv("EC2_INSTANCE_IP", "")
+EC2_INSTANCE_PORT = int(os.getenv("EC2_INSTANCE_PORT", ""))
 EC2_INSTANCE_URL = os.getenv("EC2_INSTANCE_DB", f'http://{EC2_INSTANCE_IP}:{EC2_INSTANCE_PORT}')
 
 if not MONGODB_URL:
@@ -61,3 +64,5 @@ projects_collection_name = "projects"
 environments_collection_name = "environments"
 project_members_collection_name = "project_members"
 organization_members_collection_name = "organization_members"
+encryption_keys_collection_name = "encryption_keys"
+encryption_key_versions_collection_name = "encryption_key_versions"
